e3f85b1615e917010f9547e8979982ab62bfc852
shalk <xshalk@163.com>
2019-01-03 23:14:17 +0800

    167. Two Sum II - Input array is sorted,go version

---
+++ algorithm/go/two-sum-ii-input-array-is-sorted/p167.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
+func twoSum(numbers []int, target int) []int {
+	i := 0
+	j := len(numbers) - 1
+	for i < j {
+		if numbers[i]+numbers[j] == target {
+			return []int{i + 1, j + 1}
+		} else if numbers[i]+numbers[j] > target {
+			j--
+		} else {
+			i++
+		}
+	}
+	return []int{i + 1, j + 1}
+}
